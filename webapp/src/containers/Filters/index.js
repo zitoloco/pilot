2b36eb92bbfa0b96f48d6ff9aa8fd7c42e533b9b
Derek Stavis <dekestavis@gmail.com>
2017-09-21 18:55:10 -0300

    filters: refactor filters state to props

--- webapp/src/containers/Filters/index.js
+++ webapp/src/containers/Filters/index.js
@@ -1,10 +1,13 @@
-/* eslint-disable */
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import IconArrowUp from 'react-icons/lib/md/keyboard-arrow-up'
 import IconArrowDown from 'react-icons/lib/md/keyboard-arrow-down'
 import IconFunnel from 'react-icons/lib/fa/filter'
-import { keys, merge } from 'ramda'
+import {
+  merge,
+  partial,
+} from 'ramda'
 
 import style from './style.css'
 
@@ -29,241 +32,18 @@ import CheckboxGroup from '../../components/CheckboxGroup'
 class Filters extends Component {
   constructor (props) {
     super(props)
-    
+
     this.state = {
       showContent: true,
-      dateRangeItems: [
-        {
-          value: 'today',
-          label: 'Hoje',
-        },
-        {
-          value: '7days',
-          label: '7 dias',
-        },
-        {
-          value: '15days',
-          label: '15 dias',
-        },
-        {
-          value: '30days',
-          label: '30 dias',
-        },
-        {
-          value: '60days',
-          label: '60 dias',
-        },
-      ],
       selectedDate: 'hoje',
       search: '',
-      filters: {
-        paymentMethods: {
-          name: 'Formas de pagamento',
-          selected: [],
-          all: [
-            {
-              label: 'Boleto',
-              value: 'boleto',
-            },
-            {
-              label: 'Cartão de crédito',
-              value: 'credit_card',
-            },
-            {
-              label: 'Cartão de débito',
-              value: 'debit_card',
-            },
-            {
-              label: 'Cartão de crédito estrangeiro',
-              value: 'foreign_credit_card',
-            },
-          ],
-        },
-        refuseReason: {
-          name: 'Razão de recusa',
-          selected: [],
-          all: [
-            {
-              label: 'Operadora de cartão',
-              value: 'card_acquirer',
-            },
-            {
-              label: 'Timeout de captura',
-              value: 'capture_timeout',
-            },
-            {
-              label: 'Antifraude',
-              value: 'anti_fraud',
-            },
-          ],
-        },
-        businessModel: {
-          name: 'Modelo de negócio',
-          selected: [],
-          all: [
-            {
-              label: 'Recorrência',
-              value: 'recurrence',
-            },
-          ],
-        },
-        transactionStati: {
-          name: 'Status de transação',
-          selected: [],
-          all: [
-            {
-              label: 'Paga',
-              value: 'payed',
-            },
-            {
-              label: 'Recusada',
-              value: 'refused',
-            },
-            {
-              label: 'Estornada',
-              value: 'reversed',
-            },
-            {
-              label: 'Estorno pendente',
-              value: 'pending_reversal',
-            },
-            {
-              label: 'Aguardando pagamento',
-              value: 'waiting_payment',
-            },
-            {
-              label: 'Processando',
-              value: 'processing',
-            },
-            {
-              label: 'Chargeback',
-              value: 'chargeback',
-            },
-            {
-              label: 'Chargeback reapresentado',
-              value: 'contested_chargeback',
-            },
-            {
-              label: 'Boleto pago com valor inferior',
-              value: 'boleto_less_value',
-            },
-            {
-              label: 'Esperando registro',
-              value: 'waiting_register',
-            },
-            {
-              label: 'Boleto pago com valor superior',
-              value: 'boleto_more_value',
-            },
-            {
-              label: 'Autorizada',
-              value: 'authorized',
-            },
-          ],
-        },
-        installmentNumber: {
-          name: 'Número de parcelas',
-          selected: [],
-          all: [
-            {
-              label: '1x',
-              value: '1',
-            },
-            {
-              label: '2x',
-              value: '2',
-            },
-            {
-              label: '3x',
-              value: '3',
-            },
-            {
-              label: '4x',
-              value: '4',
-            },
-            {
-              label: '5x',
-              value: '5',
-            },
-            {
-              label: '6x',
-              value: '6',
-            },
-            {
-              label: '7x',
-              value: '7',
-            },
-            {
-              label: '8x',
-              value: '8',
-            },
-            {
-              label: '9x',
-              value: '9',
-            },
-            {
-              label: '10x',
-              value: '10',
-            },
-            {
-              label: '11x',
-              value: '11',
-            },
-            {
-              label: '12x',
-              value: '12',
-            },
-          ],
-        },
-        cardBrands: {
-          name: 'Bandeiras',
-          selected: [],
-          all: [
-            {
-              label: 'Visa',
-              value: 'visa',
-            },
-            {
-              label: 'Elo',
-              value: 'elo',
-            },
-            {
-              label: 'Aura',
-              value: 'aura',
-            },
-            {
-              label: 'Amex',
-              value: 'amex',
-            },
-            {
-              label: 'Discover',
-              value: 'discover',
-            },
-            {
-              label: 'Mastercard',
-              value: 'mastercard',
-            },
-            {
-              label: 'Hipercard',
-              value: 'hipercard',
-            },
-            {
-              label: 'JCB',
-              value: 'jcb',
-            },
-            {
-              label: 'Diners',
-              value: 'diners',
-            },
-          ],
-        },
-      },
+      activeFilters: {},
     }
 
     this.handleVisibility = this.handleVisibility.bind(this)
     this.handleDateRangeChange = this.handleDateRangeChange.bind(this)
     this.handleSearchFieldChange = this.handleSearchFieldChange.bind(this)
-    this.handleChangePaymentMethods = this.handleChangePaymentMethods.bind(this)
+    this.handleFilterChange = this.handleFilterChange.bind(this)
   }
 
   handleVisibility () {
@@ -278,9 +58,12 @@ class Filters extends Component {
     this.setState({ search })
   }
 
-  handleChangePaymentMethods (index, values) {
+  handleFilterChange (filter, values) {
     this.setState({
-      [`selectedPaymentMethods${index}`]: values,
+      activeFilters: merge(
+        this.state.activeFilters,
+        { [filter]: values }
+      ),
     })
   }
 
@@ -310,7 +93,7 @@ class Filters extends Component {
             <Row flex>
               <Col>
                 <DateRange
-                  items={this.state.dateRangeItems}
+                  items={this.props.dateRanges}
                   onChange={this.handleDateRangeChange}
                   selected={this.state.selectedDate}
                 />
@@ -325,30 +108,15 @@ class Filters extends Component {
 
             {showContent &&
               <Row>
-                {keys(this.state.filters).map(filter => (
-                  <Col palm={12} tablet={6} desk={4} tv={4} key={filter}>
-                    <h4 className={style.heading}>{this.state.filters[filter].name}</h4>
+                {this.props.sections.map(({ name, items, key }) => (
+                  <Col palm={12} tablet={6} desk={4} tv={4} key={name}>
+                    <h4 className={style.heading}>{name}</h4>
                     <Row>
                       <CheckboxGroup
-                        options={this.state.filters[filter].all}
-                        name={filter}
-                        onChange={function (values) {
-                          const newValues = merge(
-                            this.state.filters[filter],
-                            { selected: values }
-                          )
-                          
-                          const state = {
-                            filters: merge(
-                              this.state.filters,
-                              { [filter]: newValues }
-                            ),
-                          }
-                          
-                          this.setState(state)
-                          
-                        }.bind(this)}
-                        values={this.state.filters[filter].selected}
+                        options={items}
+                        name={name}
+                        onChange={partial(this.handleFilterChange, [key])}
+                        values={this.state.activeFilters[key] || []}
                       />
                     </Row>
                   </Col>
@@ -360,6 +128,17 @@ class Filters extends Component {
       </Card>
     )
   }
+}
+
+Filters.propTypes = {
+  dateRanges: DateRange.propTypes.isRequired,
+  sections: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string,
+    })),
+  })).isRequired,
 }
 
 export default Filters
--- webapp/stories/Filters/index.js
+++ webapp/stories/Filters/index.js
@@ -4,7 +4,236 @@ import { action } from '@storybook/addon-actions'
 
 import Filters from '../../src/containers/Filters'
 
+const dateRanges = [
+  {
+    value: 'today',
+    label: 'Hoje',
+  },
+  {
+    value: '7days',
+    label: '7 dias',
+  },
+  {
+    value: '15days',
+    label: '15 dias',
+  },
+  {
+    value: '30days',
+    label: '30 dias',
+  },
+  {
+    value: '60days',
+    label: '60 dias',
+  },
+]
+
+const filters = [
+  {
+    key: 'payment_method',
+    name: 'Formas de pagamento',
+    items: [
+      {
+        label: 'Boleto',
+        value: 'boleto',
+      },
+      {
+        label: 'Cartão de crédito',
+        value: 'credit_card',
+      },
+      {
+        label: 'Cartão de débito',
+        value: 'debit_card',
+      },
+      {
+        label: 'Cartão de crédito estrangeiro',
+        value: 'foreign_credit_card',
+      },
+    ],
+  },
+  {
+    key: 'refuse_reason',
+    name: 'Razão de recusa',
+    items: [
+      {
+        label: 'Operadora de cartão',
+        value: 'card_acquirer',
+      },
+      {
+        label: 'Timeout de captura',
+        value: 'capture_timeout',
+      },
+      {
+        label: 'Antifraude',
+        value: 'anti_fraud',
+      },
+    ],
+  },
+  {
+    key: 'status',
+    name: 'Modelo de negócio',
+    items: [
+      {
+        label: 'Recorrência',
+        value: 'recurrence',
+      },
+    ],
+  },
+  {
+    key: 'status',
+    name: 'Status de transação',
+    items: [
+      {
+        label: 'Paga',
+        value: 'payed',
+      },
+      {
+        label: 'Recusada',
+        value: 'refused',
+      },
+      {
+        label: 'Estornada',
+        value: 'reversed',
+      },
+      {
+        label: 'Estorno pendente',
+        value: 'pending_reversal',
+      },
+      {
+        label: 'Aguardando pagamento',
+        value: 'waiting_payment',
+      },
+      {
+        label: 'Processando',
+        value: 'processing',
+      },
+      {
+        label: 'Chargeback',
+        value: 'chargeback',
+      },
+      {
+        label: 'Chargeback reapresentado',
+        value: 'contested_chargeback',
+      },
+      {
+        label: 'Boleto pago com valor inferior',
+        value: 'boleto_less_value',
+      },
+      {
+        label: 'Esperando registro',
+        value: 'waiting_register',
+      },
+      {
+        label: 'Boleto pago com valor superior',
+        value: 'boleto_more_value',
+      },
+      {
+        label: 'Autorizada',
+        value: 'authorized',
+      },
+    ],
+  },
+  {
+    key: 'installments',
+    name: 'Número de parcelas',
+    items: [
+      {
+        label: '1x',
+        value: '1',
+      },
+      {
+        label: '2x',
+        value: '2',
+      },
+      {
+        label: '3x',
+        value: '3',
+      },
+      {
+        label: '4x',
+        value: '4',
+      },
+      {
+        label: '5x',
+        value: '5',
+      },
+      {
+        label: '6x',
+        value: '6',
+      },
+      {
+        label: '7x',
+        value: '7',
+      },
+      {
+        label: '8x',
+        value: '8',
+      },
+      {
+        label: '9x',
+        value: '9',
+      },
+      {
+        label: '10x',
+        value: '10',
+      },
+      {
+        label: '11x',
+        value: '11',
+      },
+      {
+        label: '12x',
+        value: '12',
+      },
+    ],
+  },
+  {
+    key: 'card_brand',
+    name: 'Bandeiras',
+    items: [
+      {
+        label: 'Visa',
+        value: 'visa',
+      },
+      {
+        label: 'Elo',
+        value: 'elo',
+      },
+      {
+        label: 'Aura',
+        value: 'aura',
+      },
+      {
+        label: 'Amex',
+        value: 'amex',
+      },
+      {
+        label: 'Discover',
+        value: 'discover',
+      },
+      {
+        label: 'Mastercard',
+        value: 'mastercard',
+      },
+      {
+        label: 'Hipercard',
+        value: 'hipercard',
+      },
+      {
+        label: 'JCB',
+        value: 'jcb',
+      },
+      {
+        label: 'Diners',
+        value: 'diners',
+      },
+    ],
+  },
+]
+
 storiesOf('Filters', module)
   .add('Default', () => (
-    <Filters />
+    <Filters
+      dateRanges={dateRanges}
+      sections={filters}
+    />
   ))
