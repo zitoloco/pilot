cac9a295d8d08e43b714ec51ea1090385f6fc787
Derek Stavis <dekestavis@gmail.com>
2017-09-26 09:14:57 -0300

    form: add fieldset style

--- webapp/src/containers/Form/index.js
+++ webapp/src/containers/Form/index.js
@@ -125,7 +125,7 @@ export class Form extends Component {
   render () {
     if (this.props.fieldset) {
       return (
-        <fieldset>
+        <fieldset style={{ border: 0, paddingRight: 0 }}>
           {this.props.children}
         </fieldset>
       )
