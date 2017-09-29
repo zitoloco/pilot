8e20e3df38a5698c908e3cde53244ff4f8417b80
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2017-09-29 15:40:54 -0300

    charts: add legend to victory

--- webapp/stories/Graphs/victory.js
+++ webapp/stories/Graphs/victory.js
@@ -14,6 +14,7 @@ import {
   VictoryLabel,
   VictoryStack,
   VictoryBar,
+  VictoryLegend,
 } from 'victory'
 
 import moment from 'moment'
@@ -96,7 +97,7 @@ const generateLines = (type) => {
           <VictoryLine />
 
           <VictoryScatter
-            size={(d, a) => {return a ? 8 : 3;}}
+            size={(d, a) => {return a ? 8 : 3}}
           />
         </VictoryGroup>
       )
@@ -134,6 +135,8 @@ storiesOf('Graphs/victory', module)
         domainPadding={15}
         containerComponent={<VictoryVoronoiContainer />}
       >
+        <VictoryLegend />
+
         {generateLines('line')}
       </VictoryChart>
     )
@@ -153,6 +156,8 @@ storiesOf('Graphs/victory', module)
         <VictoryAxis
           dependentAxis
         />
+
+        <VictoryLegend />
 
         <VictoryStack>
           {generateLines('stacked')}
